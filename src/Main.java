045ee8121372cabd14e0f6cde0d1e2e5917650d3
Asus4mmad <parsaparsa02198@gmail.com>
2026-02-13 22:35:15 +0330

    main java change

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import edu.ticket.Ticket;
 import edu.ticket.TicketService;
+import edu.ticket.factory.TicketFactory;
+import edu.ticket.factory.TicketFactoryProvider;
 
 public class Main {
-
     public static void main(String[] args) {
+        TicketService service = new TicketService();
 
-        TicketService ticketService = new TicketService();
+        System.out.println("=== Processing Bug Ticket from Web ===");
+        TicketFactory bugFactory = TicketFactoryProvider.getFactory("BUG");
+        Ticket bugTicket = bugFactory.createTicket(1, "WEB");
+        service.processTicket(bugTicket);
 
-        // Example 1: Bug reported from web
-        Ticket ticket = new Ticket(1,"WEB", "BUG");
-        ticket.setRequest("I see a very very BAD BUG!");
-        ticketService.handle(ticket);
+        System.out.println("\n=== Processing Generic Ticket from Email ===");
+        TicketFactory genericFactory = TicketFactoryProvider.getFactory("GENERIC");
+        Ticket genericTicket = genericFactory.createTicket(2, "EMAIL");
+        service.processTicket(genericTicket);
     }
 }
